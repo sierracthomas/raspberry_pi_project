3432b9572f156af5481c58c3764f305c5a9ee7da
AnanyaBandopadhyay <anneqms11@gmail.com>
2022-12-04 15:42:10 -0500

    Update code for communication between raspberry pi(s)

--- pico_codes/communicate_data_b.py
+++ pico_codes/communicate_data_b.py
@@ -1,4 +1,5 @@
 # sending data points for ball tracker
+# led_server
 
 from easy_comms import Easy_comms
 from time import sleep
@@ -6,48 +7,64 @@ import time
 from machine import Pin, ADC
 import json
 
+print(int(time.time()*1000))
+sleep(0.1)
+print(int(time.time()*1000))
+
 hrs=time.localtime()[3]
 mins=time.localtime()[4]
 secs=time.localtime()[5]
 m_secs=time.localtime()[6]
-print (str(hrs)+":"+str(mins)+":"+str(secs)+":"+str(m_secs))
 command = {'command':'blink', 'args': 'on'}
 conversion_factor = (1 / 6553) * 10
 
 
 com1 = Easy_comms(0,9600)
-calibration_pin=ADC(26).read_u16()
-percent0 = calibration_pin * conversion_factor
-prcalib26b = adc_28 * conversion_factor
+calibration_pin=ADC(28).read_u16()
+
+prcalib28b = calibration_pin * conversion_factor
 #sending calibration value
-com1.send(str(prcalib26b))
+com1.send(str(prcalib28b))
 pr27b=" "
-pr28b=" "
+pr26b=" "
+adc_26=ADC(26).read_u16()
+percent26b = adc_26 * conversion_factor
+print(percent26b)
+adc_27=ADC(27).read_u16()
+percent27b = adc_27 * conversion_factor
+print(percent27b)    
+    
+    
 #first resistor
+while True:
+    #com1.send(str(56.0))
+    adc_26=ADC(26).read_u16()
+    percent26b = adc_26 * conversion_factor
+    
+    hrs=time.localtime()[3]
+    mins=time.localtime()[4]
+    secs=time.localtime()[5]
+    m_secs=time.localtime()[6]
+    if(abs(percent26b-prcalib28b)>=3.0):
+        print(percent26b)
+        pr26b=(str(abs(percent26b-prcalib28b))+" "+str(secs))
+        break
+#second resistor
 while True:
     #com1.send(str(56.0))
     adc_27=ADC(27).read_u16()
     percent27b = adc_27 * conversion_factor
-
+    
     hrs=time.localtime()[3]
     mins=time.localtime()[4]
     secs=time.localtime()[5]
     m_secs=time.localtime()[6]
-    if(abs(percent27b-prcalib26b)>=2.0):
-        pr27b=(str(abs(percent27b-prcalib26b))+" "+str(hrs)+":"+str(mins)+":"+str(secs)+":"+str(m_secs))
+    if(abs(percent27b-prcalib28b)>=3.0):
+        print(percent27b)
+        pr27b=(str(abs(percent27b-prcalib28b))+" "+str(secs))
         break
     
-#second resistor
-while True:
-    #com1.send(str(56.0))
-    adc_28=ADC(28).read_u16()
-    percent28b = adc_28 * conversion_factor
-
-    hrs=time.localtime()[3]
-    mins=time.localtime()[4]
-    secs=time.localtime()[5]
-    m_secs=time.localtime()[6]
-    if(abs(percent28b-prcalib26b)>=2.0):
-        pr28b=(str(abs(percent28b-prcalib26b))+" "+str(hrs)+":"+str(mins)+":"+str(secs)+":"+str(m_secs))
-        com1.send(pr27b+"\n"+pr28b)
-        break
+com1.send(pr26b)
+sleep(2)
+com1.send(pr27b)
+    
