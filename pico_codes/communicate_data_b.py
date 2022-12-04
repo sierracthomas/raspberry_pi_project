8fd45c424959a2332f8c6cefa830e28df1c9f980
AnanyaBandopadhyay <anneqms11@gmail.com>
2022-12-04 13:49:09 -0500

    Update code for communication between raspberry pi(s)

--- pico_codes/communicate_data_b.py
+++ pico_codes/communicate_data_b.py
@@ -1,4 +1,4 @@
-# led_server
+# sending data points for ball tracker
 
 from easy_comms import Easy_comms
 from time import sleep
@@ -16,48 +16,38 @@ conversion_factor = (1 / 6553) * 10
 
 
 com1 = Easy_comms(0,9600)
-led = Pin("LED", Pin.OUT)
-adc_28=ADC(28).read_u16()
-calibration_pin=ADC(27).read_u16()
+calibration_pin=ADC(26).read_u16()
 percent0 = calibration_pin * conversion_factor
-percent1 = adc_28 * conversion_factor
+prcalib26b = adc_28 * conversion_factor
 #sending calibration value
-com1.send(str(percent0))
-print(adc_28)
-str_1=" "
-str_2=" "
+com1.send(str(prcalib26b))
+pr27b=" "
+pr28b=" "
 #first resistor
 while True:
     #com1.send(str(56.0))
-    adc_28=ADC(28).read_u16()
-    calibration_pin=ADC(27).read_u16()
-    percent0 = calibration_pin * conversion_factor
-    percent1 = adc_28 * conversion_factor
+    adc_27=ADC(27).read_u16()
+    percent27b = adc_27 * conversion_factor
 
     hrs=time.localtime()[3]
     mins=time.localtime()[4]
     secs=time.localtime()[5]
     m_secs=time.localtime()[6]
-    print(percent1)
-    if(abs(percent1-percent0)>=2.0):
-        str_1=(str(percent1-percent0)+" "+str(hrs)+":"+str(mins)+":"+str(secs)+":"+str(m_secs))
+    if(abs(percent27b-prcalib26b)>=2.0):
+        pr27b=(str(abs(percent27b-prcalib26b))+" "+str(hrs)+":"+str(mins)+":"+str(secs)+":"+str(m_secs))
         break
     
 #second resistor
 while True:
     #com1.send(str(56.0))
-    adc_26=ADC(26).read_u16()
-    calibration_pin=ADC(27).read_u16()
-    percent0 = calibration_pin * conversion_factor
-    percent1 = adc_26 * conversion_factor
+    adc_28=ADC(28).read_u16()
+    percent28b = adc_28 * conversion_factor
 
     hrs=time.localtime()[3]
     mins=time.localtime()[4]
     secs=time.localtime()[5]
     m_secs=time.localtime()[6]
-    print(percent1)
-    if(abs(percent1-percent0)>=2.0):
-        str_2=(str(percent1-percent0)+" "+str(hrs)+":"+str(mins)+":"+str(secs)+":"+str(m_secs))
-        com1.send(str_1+"\n"+str_2)
-        break    
-sleep(10)
+    if(abs(percent28b-prcalib26b)>=2.0):
+        pr28b=(str(abs(percent28b-prcalib26b))+" "+str(hrs)+":"+str(mins)+":"+str(secs)+":"+str(m_secs))
+        com1.send(pr27b+"\n"+pr28b)
+        break
